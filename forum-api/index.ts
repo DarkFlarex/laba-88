063abe80590656d2bbfc4d4758c0a73ffb7c7a95
DarkFlarex <darkflarexxx@gmail.com>
2024-09-20 13:30:34 +0600

    Добавляем бэк

---
+++ forum-api/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import config from './config';
+import * as mongoose from 'mongoose';
+import usersRouter from "./routers/users";
+import postsRouter from "./routers/posts";
+import commentsRouter from "./routers/comments";
+
+const app = express();
+const port = 8000;
+
+app.use(cors(config.corsOptions));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
+
+const run = async () => {
+  await mongoose.connect(config.database);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
+

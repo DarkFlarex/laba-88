4b323368d24ebad4e37f5a817928b79f33a840e6
DarkFlarex <darkflarexxx@gmail.com>
2024-09-20 14:02:57 +0600

    Добавлен post запрос для комментариев. Проверка авторизации, если пользователь не найден, возвращается 401. Проверка существования поста по postId. Проверка наличия текста комментария

--- forum-api/routers/comments.ts
+++ forum-api/routers/comments.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import Post from "../models/Post";
+import auth, {RequestWithUser} from "../middleware/auth";
 import Comment from '../models/Comment';
+import mongoose from "mongoose";
 
 const commentsRouter = express.Router();
 
@@ -15,6 +18,38 @@ commentsRouter.get('/', async (req, res, next) => {
         return res.send(comments);
     } catch (error) {
         next(error);
+    }
+});
+
+commentsRouter.post('/', auth ,async (req: RequestWithUser,res,next) => {
+  try {
+    if (!req.user) {
+        return res.status(401).send({ error: 'User not found' });
+    }
+
+    const post = await Post.findById(req.body.postId);
+
+    if (!post) {
+        return res.status(404).send({ error: 'Post not found!' });
+    }
+
+      if (!req.body.comment) {
+          return res.status(404).send({ error: 'comment not found!' });
+      }
+
+        const comment = new Comment({
+            userPosted: req.user._id,
+            postId: req.body.postId,
+            comment: req.body.comment,
+            datetime: new Date(),
+        });
+        await comment.save();
+        return res.send(comment);
+    } catch (error){
+        if (error instanceof mongoose.Error.ValidationError){
+            return  res.status(400).send(error);
+        }
+        return next(error);
     }
 });
 
